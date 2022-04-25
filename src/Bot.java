1d03eb3e505966b80a669d0377deb30af9e97b26
RuslanRamz <zk11318@gmail.com>
2022-04-25 17:39:30 +0300

    bot

--- src/Bot.java
+++ src/Bot.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 
 public class Bot {
     public static void main(String[] args) throws IOException {
-        Scanner console = new Scanner(System.in);
-        String name = console.next();
-        String age = console.next();
+        while (true) {
+            System.out.print("Впиши фразу из 2х слов: ");
 
-        System.out.println(age +" не " + name);
+            Scanner console = new Scanner(System.in);
+
+            String st1 = console.next();
+            String st2 = console.next();
+
+            System.out.println(st2 + " не " + st1);
+            if ("stop".equals(st1)){
+            break;}
+        }
+
 
 
     }
