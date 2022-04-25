6766e7cd9003fc431dbaab4a8e47c67c7d1ed339
RuslanRamz <zk11318@gmail.com>
2022-04-25 17:53:03 +0300

    bot

--- src/Bot.java
+++ src/Bot.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Bot {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         while (true) {
             System.out.print("Впиши фразу из 2х слов: ");
 
